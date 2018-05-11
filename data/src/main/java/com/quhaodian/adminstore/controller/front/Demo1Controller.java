ca87028f24b894bd7f2f0b143e850c1a95bc7b62
ada.young <cng1985@gmail.com>
2018-05-11 17:02:24 +0800

    移除测试文件

--- data/src/main/java/com/quhaodian/adminstore/controller/front/Demo1Controller.java
+++
@@ -1,9 +0,0 @@
-package com.quhaodian.adminstore.controller.front;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RequestMapping("/demo1")
-@Controller
-public class Demo1Controller extends TemplateController {
-}
--- data/src/main/java/com/quhaodian/adminstore/controller/front/Demo2Controller.java
+++
@@ -1,9 +0,0 @@
-package com.quhaodian.adminstore.controller.front;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-@RequestMapping("/demo2")
-@Controller
-public class Demo2Controller extends TemplateController {
-}
