0289ce39481ebfe1b1d1ac8ebc8876ee6e79ad85
ada.young <cng1985@gmail.com>
2018-06-11 14:31:25 +0800

    修改用户模块

--- data/src/main/java/com/quhaodian/adminstore/controller/rest/MemberRestController.java
+++ data/src/main/java/com/quhaodian/adminstore/controller/rest/MemberRestController.java
@@ -3,7 +3,9 @@ package com.quhaodian.adminstore.controller.rest;
 
 import com.quhaodian.adminstore.rest.api.MemberApi;
 import com.quhaodian.adminstore.rest.domain.page.MemberPage;
+import com.quhaodian.adminstore.rest.domain.request.MemberUpdateRequest;
 import com.quhaodian.discover.rest.base.RequestTokenPageObject;
+import com.quhaodian.discover.rest.base.ResponseObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +17,11 @@ public class MemberRestController {
   @RequestMapping("/member/page")
   public MemberPage page(RequestTokenPageObject request) {
     return api.page(request);
+  }
+  
+  @RequestMapping("/member/update")
+  public ResponseObject update(MemberUpdateRequest request) {
+    return api.update(request);
   }
   
   @Autowired
