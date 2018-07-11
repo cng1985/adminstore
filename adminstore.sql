27ae737b1f403922e7d4ae8b8840e0f545a90c41
ada.young <cng1985@gmail.com>
2018-07-11 21:21:45 +0800

    修改sql文件

--- adminstore.sql
+++ adminstore.sql
@@ -1,19 +1,4 @@
--- --------------------------------------------------------
--- 主机:                           127.0.0.1
--- 服务器版本:                        5.5.47 - MySQL Community Server (GPL)
--- 服务器操作系统:                      Win32
--- HeidiSQL 版本:                  9.3.0.4984
--- --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
--- 导出 adminstore 的数据库结构
-DROP DATABASE IF EXISTS `adminstore`;
-CREATE DATABASE IF NOT EXISTS `adminstore` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `adminstore`;
 
 
 -- 导出  表 adminstore.area 结构
