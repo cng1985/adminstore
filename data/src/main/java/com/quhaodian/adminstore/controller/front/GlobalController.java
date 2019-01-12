c199b13efc92719b4af2fabc0020353b3e3bff2c
ada.young <cng1985@gmail.com>
2019-01-12 21:39:10 +0800

    修改异常记录

--- data/src/main/java/com/quhaodian/adminstore/controller/front/GlobalController.java
+++ data/src/main/java/com/quhaodian/adminstore/controller/front/GlobalController.java
@@ -1,23 +1,25 @@
 package com.quhaodian.adminstore.controller.front;
 
 
+import com.haoxuer.discover.rest.base.ResponseObject;
+import com.haoxuer.discover.user.utils.UserUtils;
 import com.quhaodian.adminstore.data.entity.ExceptionLog;
 import com.quhaodian.adminstore.data.service.ExceptionLogService;
 import com.quhaodian.adminstore.exception.NoDataException;
 import com.quhaodian.adminstore.exception.NoUserTokenException;
 import com.quhaodian.adminstore.exception.UnAuthorizationException;
-import com.haoxuer.discover.rest.base.ResponseObject;
-import com.haoxuer.discover.user.utils.UserUtils;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-
+import org.apache.shiro.authz.AuthorizationException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.CannotAcquireLockException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.Map;
@@ -25,40 +27,30 @@ import java.util.Set;
 
 
 @ControllerAdvice
-public class GlobalController  {
+public class GlobalController   {
+
+
 
   @Autowired
   ExceptionLogService logService;
 
-  @ResponseBody
   @ExceptionHandler(ServletException.class)
-  public ResponseObject expx(HttpServletRequest request, Exception ex) {
-    ResponseObject result = new ResponseObject();
-    result.setMsg("服务器异常!");
-    result.setCode(-1);
-    savaLog(ex, request);
-    return result;
+  public ModelAndView expx(HttpServletRequest request, Exception ex) {
+    return handleException(request, ex);
+  }
+  @ExceptionHandler(DataIntegrityViolationException.class)
+  public ModelAndView dataIntegrityViolationException(HttpServletRequest request, Exception ex) {
+    return handleException(request, ex);
+  }
+  @ExceptionHandler(IllegalArgumentException.class)
+  public ModelAndView illegalArgument(HttpServletRequest request, Exception ex) {
+    return handleException(request, ex);
   }
 
-  @ResponseBody
-  @ExceptionHandler(CannotAcquireLockException.class)
-  public ResponseObject cannotacquirelock(HttpServletRequest request, Exception ex) {
-    ResponseObject result = new ResponseObject();
-    result.setMsg("事物异常!");
-    result.setCode(-1);
-    savaLog(ex, request);
-    return result;
-  }
-
-
-  @ResponseBody
   @ExceptionHandler(NullPointerException.class)
-  public ResponseObject nullEx(HttpServletRequest request, Exception ex) {
-    ResponseObject result = new ResponseObject();
-    result.setMsg("数据处理异常!");
-    result.setCode(-1);
-    savaLog(ex, request);
-    return result;
+  public ModelAndView nullEx(HttpServletRequest request, Exception ex) {
+    return handleException(request, ex);
+
   }
 
   @ResponseBody
@@ -70,7 +62,7 @@ public class GlobalController  {
     savaLog(ex, request);
     return result;
   }
-  
+
   @ResponseBody
   @ExceptionHandler(NoUserTokenException.class)
   public ResponseObject noUserToken(HttpServletRequest request, Exception ex) {
@@ -80,17 +72,18 @@ public class GlobalController  {
     savaLog(ex, request);
     return result;
   }
-  
-  
+
+
   @ResponseBody
   @ExceptionHandler(UnAuthorizationException.class)
   public ResponseObject unAuthorization(HttpServletRequest request, Exception ex) {
     ResponseObject result = new ResponseObject();
     result.setMsg("用户token失效!");
     result.setCode(-3);
+    savaLog(ex, request);
     return result;
   }
-  
+
   @ResponseBody
   @ExceptionHandler(UserUtils.TokenInvalidException.class)
   public ResponseObject tokenInvalidException(HttpServletRequest request, Exception ex) {
@@ -100,10 +93,34 @@ public class GlobalController  {
     savaLog(ex, request);
     return result;
   }
-  
-  
-  public GlobalController() {
 
+  public static boolean isAjaxRequest(HttpServletRequest request) {
+    String requestedWith = request.getHeader("x-requested-with");
+    if (requestedWith != null && requestedWith.equalsIgnoreCase("XMLHttpRequest")) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @ExceptionHandler(AuthorizationException.class)
+  public ModelAndView unauthenticatedException(HttpServletRequest request, Exception ex) {
+    return handleException(request, ex);
+
+  }
+
+  private ModelAndView handleException(HttpServletRequest request, Exception ex) {
+    savaLog(ex, request);
+    if (isAjaxRequest(request)) {
+      ModelAndView mav = new ModelAndView(new MappingJackson2JsonView());
+      mav.addObject("code", -5);
+      mav.addObject("msg", "没有登陆");
+      return mav;
+    } else {
+      RedirectView redirectView=new RedirectView("/index.htm",true);
+      ModelAndView mav = new ModelAndView(redirectView);
+      return mav;
+    }
   }
 
   private void savaLog(Exception ex, HttpServletRequest request) {
@@ -142,5 +159,8 @@ public class GlobalController  {
       }
     }
     return ret;
+  }
+  public GlobalController() {
+    //super("callback");
   }
 }
